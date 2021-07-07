b1204018d042c4312fd0d3061bbecca97464f859
SatoroDEV <satoroswap@gmail.com>
2021-07-07 16:27:32 +0200

    add adgnz

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.94f0c81f69182f069eebdfd767badb30.js"
+  "./precache-manifest.b37a70fe47f48a76182db38cf932e3a0.js"
 );
 
 self.addEventListener('message', (event) => {
